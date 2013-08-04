e2dbced6980c5db70934e61565a02f522f1f463e
Ubuntu <ubuntu@ip-172-31-20-31.sa-east-1.compute.internal>
2013-08-04 21:28:45 +0000

    Part 1 of assignement

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var content;
+// First I want to read the file
+fs.readFile(__dirname + '/index.html', 'utf8',
+function (err, data) {
+ if (err) {
+    return console.log(err);
+  }
+  content = data;
 });
 
-var port = process.env.PORT || 5000;
+
+app.get('/', function(request, response) {
+  response.send(content);
+});
+
+var port = process.env.PORT || 81;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
